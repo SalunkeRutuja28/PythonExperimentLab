c3a778665ec7693ab8443f74f0b483cc6a8e9701
RutujaDS <30453335+SalunkeRutuja28@users.noreply.github.com>
2026-07-20 17:11:24 +0530

    Add files via upload
    int float

---
+++ Week 0/calculator.py
@@ -0,0 +1,21 @@
+# x = input("What's x? ")
+# y = input("What's y? ")
+
+# z = x + y
+# z = int(x) + int(y)
+# print(z)
+
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
+# print(x + y)
+
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+print(round(x + y))
+z = round(x + y)
+print(f"{z:,}")
+
+a = round( x / y, 2)
+a = x / y
+print(a)
+print(f"{a:.2f}")
